b1365413557bd68bc67126b752e49ca37cf8e56b
IngSoto <v.ivnsto@hotmail.com>
2019-10-20 05:05:13 -0500

    PrimeraVersionEmpleadoComisionPrueba

---
+++ Clientes/EmpleadoComision.java
@@ -0,0 +1,19 @@
+public class EmpleadoComision{
+	private int ventasTotales;
+	private float tasaComision;
+
+	public EmpleadoComision(String nombre, String apellido, String numSeguridad, int ventasTotales, float tasaComision){
+		super(nombre, apellido, numSeguridad);
+		this.ventasTotales = ventasTotales;
+		this.tasaComision = tasaComision;
+	}
+
+	public abstract float ingresos(float salarioSemanal){
+
+	}
+
+	@Override
+	public String toString(){
+		return "Empleado por comisión [" + getNombre() + " " + getApellido() + ". SegSocial: " + getNumSeguridad() + ". Sueldo: " + getIngresos() + "]";
+	}
+}
